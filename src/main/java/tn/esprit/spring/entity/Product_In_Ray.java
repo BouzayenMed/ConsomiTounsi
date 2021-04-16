71b0f7325bc6d90a976cb91158e200b0e3d38385
BouzayenMed <mohamed.bouzayen1@esprit.tn>
2021-04-16 23:48:47 +0100

    Boj2

--- src/main/java/tn/esprit/spring/entity/Product_In_Ray.java
+++ src/main/java/tn/esprit/spring/entity/Product_In_Ray.java
@@ -2,13 +2,13 @@ package tn.esprit.spring.entity;
 
 
 
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.UniqueConstraint;
+
 
 @Entity
 
